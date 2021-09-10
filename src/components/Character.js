952c3b934e7474a10285f78b19982ddd00402e78
tragiclov <frijacblu@yahoo.com>
2021-09-10 15:49:53 -0700

    I dont know whats happening

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,31 @@
 // Write your Character component here
 import React, {useState} from 'react'
 import styled from 'styled-components'
+const CharacterCard = styled.div`
+background-color : white;
+opacity: 65%;
+width: 50%;
+display: block;
+margin: 4em auto;
+padding: 10px;
+`
+const StyledName = styled.h1`
+    font-size: 2em;
+    `
+const StyledH2 = styled.h2`
+    font-size: 1em;
+    `
 
 const Character = (props) => {
     const {characters} = props;
 return (
     <div>
-        {characters.map(res =>{
+        {characters.map(res, index =>{
             return(
-                <div>
-                    <h1>{res.name}</h1>
-                    <h2>{res.birth_year}</h2>
-                    <h2>{res.gender}</h2>
-                </div>
+                <CharacterCard key = {index}>
+                    <StyledName>{res.name}</StyledName>
+                    <StyledH2>Date of Birth:</StyledH2>
+                </CharacterCard>
             )
         })}
     </div>
