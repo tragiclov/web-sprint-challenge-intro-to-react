ce8696c2a0a5a4e39f534bf0093e1b2298919d8c
tragiclov <frijacblu@yahoo.com>
2021-09-10 15:33:26 -0700

    App still broken

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const Character = (props) => {
+    const {characters} = props;
+return (
+    <div>
+        {characters.map(res =>{
+            return(
+                <div>
+                    <h1>{res.name}</h1>
+                    <h2>{res.birth_year}</h2>
+                    <h2>{res.gender}</h2>
+                </div>
+            )
+        })}
+    </div>
+)
+}
+export default Character
