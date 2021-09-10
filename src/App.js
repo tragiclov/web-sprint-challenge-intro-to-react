ac9f6aa967f870786206d503b7477c6beb05b62a
tragiclov <frijacblu@yahoo.com>
2021-09-10 16:01:10 -0700

    Everything is coded right App is broken

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import axios from 'axios';
 import {BASE_URL} from "./constants"
 import Character from "./components/Character"
+import styled from 'styled-components';
+import images from './images/ewok.jpg'
+
+const StyledBackground = styled.div`
+  background-image: url(${images});
+  background-size: cover;
+  padding: 50px;
+  `
+
+  const StyledHeader = styled.h1`
+    font-size: 4em;
+    `
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState({})
+  const [characters, setCharacters] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -19,11 +31,10 @@ const App = () => {
     }).catch(err => console.log(err))
   }, [])
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-      <p className="Characters">
-      </p>
-    </div>
+    <StyledBackground className="App">
+      <StyledHeader className="Header">Characters</StyledHeader>
+        <Character className="Characters" characters={characters}/>
+    </StyledBackground>
   )
 }
 
