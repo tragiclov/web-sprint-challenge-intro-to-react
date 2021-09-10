e5a97e6449e1d8aad12cd1f0f482073bea7b76b6
tragiclov <frijacblu@yahoo.com>
2021-09-10 15:01:33 -0700

    I think app is broken

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
-import styled from 'styled-components';
-import Character from './components/Character';
-import Details from './components/Details';
-import { response } from 'msw';
-import { set } from 'msw/lib/types/context';
+import {BASE_URL} from "./constants"
+import Character from "./components/Character"
 
-export default function App (){
+const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState({})
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [characters, setCharacters] = useState(null);
-  const [currentCharacter, setCurrentCharacter] = useState(null);
-  const [error, setError] = useState(null);
-
-
-  const openDetails = (id) =>{
-  const character = characters.filter(item => item.id === id);
-  setCurrentCharacter(character);
-};
-  const closeDetails = () =>{
-    setCurrentCharacter(null);
-  };
-  useEffect(() => {
-    console.log("Getting the characters...");
-    axios.get("https://swapi.dev/api/people")
-    .then(response => {
-      const characters = response.data;
-      let id = 1;
-      characters.forEach(item => item.id = id++);
-      console.log(characters);
-      setCharacters(characters);
-    })
-    .catch(err =>{
-      console.error(err);
-      setError(err);
-    })
-  });
+  useEffect(()=>{
+    axios.get(`${BASE_URL}`)
+    .then(res => {
+      console.log(res);
+      setCharacters(res.data);
+    }).catch(err => console.log(err))
+  }, [])
+  return (
+    <div className="App">
+      <h1 className="Header">Characters</h1>
+      <p className="Characters">
+      </p>
+    </div>
+  )
 }
 
-
+  
