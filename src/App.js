b370286278aa6b952f4599a8f8f613a86bac3b92
tragiclov <frijacblu@yahoo.com>
2021-09-10 13:39:43 -0700

    starting app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+import Character from './components/Character';
+import Details from './components/Details';
 
-const App = () => {
+export default function App (){
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [characters, setCharacters] = useState(null);
+  const [currentCharacter, setCurrentCharacter] = useState(null);
+  const [error, setError]
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
